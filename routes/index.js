367dc43b3848750c7ff8d20f5300ce819c79be19
Abhay Shah <seraski2021@gmail.com>
2025-12-25 11:34:37 +0545

    added the origin of the system

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,7 @@ app.use(cookieParser());
 
 app.use(cors({
     origin: [
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "https://crmnursing.smsitsolutions.com.au/",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
